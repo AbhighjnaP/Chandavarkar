026cbb206fe3476a21bd375f19f863458fd17050
Abhighjna Chandavarkar <abhighjnap.xworks@gmail.com>
2022-09-12 12:57:59 +0530

    I am Abhghjna P Chandavarkar

---
+++ Dummy.java
@@ -0,0 +1,74 @@
+class Wife{
+	
+	static void name() {
+		
+		System.out.println("Anu");
+		
+	}
+	
+	static void age() {
+		
+		System.out.println("age is 23");
+		
+	}
+	
+	static void speak() {
+		
+		System.out.println("can speak kannada,english,hindi");
+		
+	}
+	
+	static void prepare(){
+		
+		System.out.println("prepares food daily");
+		
+	}
+	
+	static void walks() {
+		
+		System.out.println("walks 3km daily");
+		
+	}
+	
+	static void wash() {
+		
+		System.out.print("washes dishes");
+		
+	}
+	
+	static void occupation() {
+		
+		System.out.println("telecommunication enginner");
+		
+	}
+	
+	static void hobbies() {
+		
+		System.out.println("singing");
+		
+	}
+	
+	static void fight() {
+		
+		System.out.println("she never fights");
+		
+	}
+	
+	static void type(){
+		
+		System.out.println("very talkative");
+		
+	}
+	
+	static void work(){
+		
+		System.out.println("works always");
+		
+	}
+	
+	static void travel(){
+		
+		System.out.println("travels in scooty");
+		
+	}
+}
