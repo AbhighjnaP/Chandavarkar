d100577b217bd0d43ce4543931a4aaebf3ec3450
Abhighjna Chandavarkar <abhighjnap.xworks@gmail.com>
2022-09-15 18:33:34 +0530

    Second Program

---
+++ NameByMobileNumber.java
@@ -0,0 +1,58 @@
+class NameByMobileNumber{
+	static String getNameByMobile(long number)
+{
+	System.out.println("Entered getNameByMobile");
+	    
+		if (number==9590029663L) //1
+	{
+		System.out.println("Mobile Number is :8277738399");
+		return "Abhighjna";
+	}
+		if(number==9845792152L) //2
+	{
+		System.out.println("Mobile Number is :9448722122");
+		return "Kamala";
+	}
+		if (number==9902206621L) //3
+	{
+		System.out.println("Mobile Number is :9481064199");
+		return "Prashanth";
+	}
+		if(number==9606730951L) //4
+	{
+		System.out.println("Mobile Number is :9743445720");
+		return "Bablu";
+	}
+		if (number==9481133595L) //5
+	{
+		System.out.println("Mobile Number is :9481535704");
+		return "Shringesh";
+	}
+		if(number==7259342543L) //6
+	{
+		System.out.println("Mobile Number is :9449594341");
+		return "Vishala";
+	}
+		if (number==9741094817L) //7
+	{
+		System.out.println("Mobile Number is :9449785848");
+		return "Saraswathi";
+	}
+		if(number==8185253658L) //8
+	{
+		System.out.println("Mobile Number is :08185253658");
+		return "Home";
+	}
+		if (number==7760545968L) //9
+	{
+		System.out.println("Mobile Number is :9449286046");
+		return "Pooja";
+	}
+		if(number==9844890373L) //10
+	{
+		System.out.println("Mobile Number is :9988776655L");
+		return "Gayatri";
+	}
+		return null;
+} 
+}
---
+++ NameByMobileNumberRunner.java
@@ -0,0 +1,13 @@
+class NameByMobileNumberRunner{
+	public static void main(String[] Name)
+	{
+		
+		System.out.println("Entered NameByMobileNumber");
+		
+		String ref=NameByMobileNumber.getNameByMobile(9481133595L);
+        System.out.println(ref);
+		
+	}
+	
+
+}
