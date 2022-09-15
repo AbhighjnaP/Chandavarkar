cc28a6bea98650a088c59ffb9c40680d1a80993a
Abhighjna Chandavarkar <abhighjnap.xworks@gmail.com>
2022-09-15 18:23:49 +0530

    My first program

---
+++ CapitalByState.java
@@ -0,0 +1,157 @@
+class CapitalByState{
+	
+	static String getCapitalByState(String state)
+	{
+		System.out.println("Entered capital by state name");
+		
+			if (state=="Karnataka")
+		{
+			System.out.println("Capital of Karnataka is");
+			return "Bengaluru";
+		}
+			if (state=="Andra Pradesh")
+		{
+			System.out.println("Capital of Andra Pradesh is");
+			return "Amaravati";
+		}
+			if (state=="Arunachala prades")
+		{
+			System.out.println("Capital of Arunachala pradesh is ");
+			return "Itanagar";
+		}
+			if (state=="Assam")
+		{
+			System.out.println("Capital of Assam is");
+			return "Dispur";
+		}
+			if (state=="Bihar")
+		{	
+			System.out.println("Capital of Bihar is ");
+			return "Patna";
+		}
+			if (state=="Chattisgarah")
+		{
+			System.out.println("Capital of Chattisgarh is ");
+			return "Raipur";
+		}
+			if (state=="Goa")
+		{
+			System.out.println("Capital of Goa is");
+			return "Panaji";
+		}
+			if (state=="Gujarath")
+		{
+			System.out.println("Capital of Gujarath is");
+			return "Gandhinagar";
+		}
+			if (state=="Haryana")
+		{
+			System.out.println("Capital of Haryana is ");
+			return "Chandigarh";
+		}
+			if (state=="Himachal Pradesh")
+		{
+			System.out.println("Capital of Himachal Pradesh is");
+			return "Shimla";
+		}
+			if (state=="Jammu and Kashmir")
+		{	
+			System.out.println("Capital of Jmmu and Kashmir is ");
+			return "Srinagar";
+		}
+			if (state=="Jharkhand")
+		{
+			System.out.println("Capital of Jharkhand is ");
+			return "Ranchi";
+		}
+			if (state=="Kerala")
+		{
+			System.out.println("Capital of Kerala is");
+			return "Trivandrum";
+		}
+			if (state=="Madhya Pradesh")
+		{
+			System.out.println(" Capital city of Madhyapradesh is");
+			return "Bhopal";
+		}
+			if (state=="Maharashtra")
+		{
+			System.out.println("Capital of Maharashtra is ");
+			return "Mumbai";
+		}
+			if (state=="Manipur")
+		{
+			System.out.println("Capital of Manipur is");
+			return "Imphal";
+		}
+			if (state=="Meghalaya")
+		{	
+			System.out.println("Capital of Meghalaya is ");
+			return "Shillong";
+		}
+			if (state=="Mizoram")
+		{
+			System.out.println("Capital of Mizoram is ");
+			return "Aizwal";
+		}
+			if (state=="Nagaland")
+		{
+			System.out.println("Capital of Nagaland is");
+			return "Kohima";
+		}
+			if (state=="Odisha")
+		{
+			System.out.println("Capital of Odisha is");
+			return "Bhubaneshwar";
+		}
+			if (state=="Punjab")
+		{
+			System.out.println("Capital of Punjab is ");
+			return "Chandigarah";
+		}
+			if (state=="Rajasthan")
+		{
+			System.out.println("Capital of Rajasthan is");
+			return "Jaipur";
+		}	
+			if (state=="Sikkhim")
+		{	
+			System.out.println("Capital of Sikkhim is ");
+			return "Gangtok";
+		}
+			if (state=="Tamil Nadu")
+		{
+			System.out.println("Capital of Tamil Nadu is ");
+			return "Chennai";
+		}
+			if (state=="Telangana")
+		{
+			System.out.println("Capital of Telangana is");
+			return "Hydrabad";
+		}
+			if (state=="Tripura")
+		{
+			System.out.println("Capital of Tripura is");
+			return "Agartala";
+		}
+			if (state=="Uttar Pradesh")
+		{
+			System.out.println("Capital of Uttar Pradesh is ");
+			return "Lucknow";
+		}
+			if (state=="Uttarakhand")
+		{
+			System.out.println("Capital of Uttarakhand is");
+			return "Dehradun";
+		}	
+			if (state=="West Bengal")
+		{
+			System.out.println("Capital of West Bengal is");
+			return "Kolkotta";
+		}	
+		
+		return null;
+	}
+	
+	
+}
