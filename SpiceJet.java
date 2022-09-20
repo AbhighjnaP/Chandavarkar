4f57d6ce9dc347596edc501acc1b3d149416f808
Abhighjna Chandavarkar <abhighjnap.xworks@gmail.com>
2022-09-20 15:59:56 +0530

    Array 1

---
+++ SpiceJet.java
@@ -0,0 +1,16 @@
+class SpiceJet{
+	
+	static void flightnos(String[] flightnos){
+		
+		System.out.println("flightnos start");
+		for (int index=0;index<flightnos.length;index++){
+			System.out.println(flightnos[index]);
+			
+		}
+		System.out.println("flightnos ends");
+		
+	}
+	
+	
+	
+}
---
+++ SpiceJetRunner.java
@@ -0,0 +1,40 @@
+class SpiceJetRunner{
+	
+	public static void main(String[] fly){
+		
+		System.out.println("main start");
+		String[] flight1={"DB647","BA1326","BA1476","GF5232","AA8025"};
+		String[] flight2={"XZ3265","SQ0972","SQ2509","R19922","XZ0569"};
+		String[] flight3={"QE0078","TY2053","ER7452","XZ0352","XZ6005"};
+		String[] flight4={"AB1365","CD4521","AF4370","BA0300","BK4311"};
+		String[] flight5={"QX2379","HA5160","BD0674","BA136","SA1476"};
+		String[] flight6={"GG128","GJ7552","EF8253","RH4521","FH5252"};
+		String[] flight7={"FG4522","FG2755","RT4586","JN78665","MG4586"};
+		String[] flight8={"TY4822","DG4582","FF4552","RG4567","RT4452"};
+		String[] flight9={"ER125","BV1257","OP4548","IU2456","CV1234"};
+		String[] flight10={"YU1452","LK7845","UI4568","HY1245","LK4589"};
+		
+		SpiceJet.flightnos(flight1);
+		SpiceJet.flightnos(flight2);
+		SpiceJet.flightnos(flight3);
+		SpiceJet.flightnos(flight4);
+		SpiceJet.flightnos(flight5);
+		SpiceJet.flightnos(flight6);
+		SpiceJet.flightnos(flight7);
+		SpiceJet.flightnos(flight8);
+		SpiceJet.flightnos(flight9);
+		SpiceJet.flightnos(flight10);
+		
+		System.out.println("Main end");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
