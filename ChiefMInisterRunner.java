912e8881cad012b10f39c7a7af1e44a21d8d99f6
Abhighjna Chandavarkar <abhighjnap.xworkz@gmail.com>
2022-09-24 18:19:00 +0530

    message

---
+++ ChiefMInisterRunner.java
@@ -0,0 +1,43 @@
+class ChiefMinisterRunner{
+
+public static void main(String[] value){
+	
+	System.out.println("Main:starts");
+	
+	String ref=value[0];
+	System.out.println("index 0:"+ref);
+	
+	String ref1=value[1];
+	System.out.println("index 0:"+ref1);
+	
+	String ref2=value[2];
+	System.out.println("index 0:"+ref2);
+	
+	String ref3=value[3];
+	System.out.println("index 0:"+ref3);
+	
+	String ref4=value[4];
+	System.out.println("index 0:"+ref4);
+	
+	String ref5=value[5];
+	System.out.println("index 0:"+ref5);
+	
+	String ref6=value[6];
+	System.out.println("index 0:"+ref6);
+	
+	String ref7=value[7];
+	System.out.println("index 0:"+ref7);
+	
+	String ref8=value[8];
+	System.out.println("index 0:"+ref8);
+	
+	String ref9=value[9];
+	System.out.println("index 0:"+ref9);
+	
+	System.out.println("Total vlaues:"+value.length);
+	
+	
+	
+}
+
+}
---
+++ riverRunner.java
@@ -0,0 +1,45 @@
+class riverRunner{
+	
+	
+	public static void main(String[] args){
+		
+	System.out.println("Main:starts");
+	
+	String ref=args[0];
+	System.out.println("index 0:"+ref);
+	
+	String ref1=args[1];
+	System.out.println("index 0:"+ref1);
+	
+	String ref2=args[2];
+	System.out.println("index 0:"+ref2);
+	
+	String ref3=args[3];
+	System.out.println("index 0:"+ref3);
+	
+	String ref4=args[4];
+	System.out.println("index 0:"+ref4);
+	
+	String ref5=args[5];
+	System.out.println("index 0:"+ref5);
+	
+	String ref6=args[6];
+	System.out.println("index 0:"+ref6);
+	
+	String ref7=args[7];
+	System.out.println("index 0:"+ref7);
+	
+	String ref8=args[8];
+	System.out.println("index 0:"+ref8);
+	
+	String ref9=args[9];
+	System.out.println("index 0:"+ref9);
+	
+	System.out.println("Total vlaues:"+args.length);
+		
+		
+	}
+
+
+
+}
